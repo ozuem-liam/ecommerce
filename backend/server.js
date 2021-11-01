d1a3040ee3651d59672d8f97994f99f294fae1fc
ozuem-liam <ozuemdw@gmail.com>
2021-11-01 14:58:05 +0100

    feat: heroku create

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/db");
 const cors = require("cors");
 const productRoutes = require("./routes/productRoutes");
 const adminRoutes = require("./routes/adminRoutes");
+const path = require('path');
 
 connectDB();
 const corsOptions = {
@@ -18,6 +19,15 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cors(corsOptions));
 app.use("/api/products", productRoutes);
 app.use("/api/admin", adminRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'frontend/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
